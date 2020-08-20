9fef15981b7aa1b9b1bfd3daf6d6c53a10f2293b
BenDunjay <bendunjay@outlook.com>
2020-08-20 22:12:27 +0100

    tests for some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :recipes
   has_many :recipes, through: :userrecipes
+
+  validates_presence_of :name
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,40 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   subject { described_class.new(name: "Ben", age: 20, favourite_mums_dish: "Spaghetti") }
+
   it "initiates a new user with a name" do
     expect(subject.name).to eq("Ben")
   end
 
   it "initiates a new user with a favourite mums's recipe" do
     expect(subject.favourite_mums_dish).to eq("Spaghetti")
+  end
+
+  it "initiates a new user with an age" do
+    expect(subject.age).to eq(20)
+  end
+
+  describe "Associations" do
+    it { should belongs_to(:userrecipes).without_validating_presence }
+  end
+
+  # context "validations" do
+  #   before { FactoryBot.build(:user) }
+
+  #   it "checks for same username" do
+  #     should validate_uniqueness_of(:username).
+  #              scoped_to(:account_id).
+  #              case_insensitive
+  #   end
+  # end
+  context "Validations" do
+    it "is valid with valid attributes" do
+      expect(User.new).to be_valid
+    end
+
+    it "is not valid without a name" do
+      user = User.new(name: nil)
+      expect(user).to_not be_valid
+    end
   end
 end
