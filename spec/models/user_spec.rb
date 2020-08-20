88a9cfd3ddd797f54c1c881d3319e8e0924086d7
BenDunjay <bendunjay@outlook.com>
2020-08-20 21:11:23 +0100

    testing working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,12 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  subject { described_class.new(name: "Ben", age: 20) }
+  subject { described_class.new(name: "Ben", age: 20, favourite_mums_dish: "Spaghetti") }
   it "initiates a new user with a name" do
     expect(subject.name).to eq("Ben")
+  end
+
+  it "initiates a new user with a favourite mums's recipe" do
+    expect(subject.favourite_mums_dish).to eq("Spaghetti")
   end
 end
