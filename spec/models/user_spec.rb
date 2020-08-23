87f5fe048e8b490f5aae1a0bd3372f6420fe55ed
BenDunjay <bendunjay@outlook.com>
2020-08-23 15:50:55 +0100

    validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,18 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   subject { described_class.new(name: "Ben", age: 20, favourite_mums_dish: "Spaghetti") }
 
-  it "initiates a new user with a name" do
-    expect(subject.name).to eq("Ben")
-  end
+  context "initiates a new user" do
+    it " with a name" do
+      expect(subject.name).to eq("Ben")
+    end
 
-  it "initiates a new user with a favourite mums's recipe" do
-    expect(subject.favourite_mums_dish).to eq("Spaghetti")
-  end
+    it " with a favourite mums's recipe" do
+      expect(subject.favourite_mums_dish).to eq("Spaghetti")
+    end
 
-  it "initiates a new user with an age" do
-    expect(subject.age).to eq(20)
+    it " with an age" do
+      expect(subject.age).to eq(20)
+    end
   end
 
   # describe "Associations" do
