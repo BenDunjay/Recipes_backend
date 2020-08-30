77e7058cd5e0dc15da3b9303671c344b6ee85a36
BenDunjay <bendunjay@outlook.com>
2020-08-30 13:58:10 +0100

    update

--- spec/models/user_recipe_spec.rb
+++ spec/models/user_recipe_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe UserRecipe, type: :model do
   let(:recipe) { instance_double(Recipe, id: 1, name: "spag bol", difficulty: "hard", cooking_time: 50, instructions: "very long instructions") }
   subject { described_class.new(user_id: user.id, recipe_id: recipe.id) }
 
-  describe "Associations" do
+  describe "Associations:" do
     it { should belong_to(:user).without_validating_presence }
     it { should belong_to(:recipe).without_validating_presence }
   end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,5 +1,11 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Users", type: :request do
-
+  user = User.create(name: "Ben", age: 20, favourite_mums_dish: "Spaghetti")
+  it "request list of all users" do
+    get users_path
+    expect(response).to be_successful
+    expect(response).to have_http_status(:success)
+    expect(response.body).to include(user.name)
+  end
 end
