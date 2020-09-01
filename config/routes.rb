e1778fd6c2b62b6857db88ae0937971ffa931479
BenDunjay <bendunjay@outlook.com>
2020-09-01 22:10:17 +0100

    adding JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :user_recipes
-  resources :recipes
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :recipes
+      resources :users
+      resources :user_recipes
+    end
+  end
 end
