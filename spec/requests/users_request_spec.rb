5c215306a239aee49e55528ae4606e9ce7e3bab4
BenDunjay <bendunjay@outlook.com>
2020-10-06 23:30:21 +0100

    added create controller test for user

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -14,10 +14,17 @@ RSpec.describe "Users", type: :request do
 
   describe "create" do
     it "creates a new user" do
-      # expect { post "/api/v1/users", params: user }.to change(User, :count).by(1)
-      expect(post "/api/v1/users", params: user).to change(User, :count).by(1)
+      user = { user: { name: "Beng", age: 20, favourite_mums_dish: "Spaghetti", username: "JG", password: "123" } }
+      post "/api/v1/users", params: user.to_json, headers: { 'Accept': "application/json", 'Content-Type': "application/json" }
+      body = JSON.parse(response.body)
       expect(response).to have_http_status :created
-      # expect(response.headers['Location']).to eq api_v1_bathroom_url(Bathroom.last)
+    end
+
+    it "changes the count of User by 1" do
+      pending "test not yet implemented"
+      user = User.create(name: "Dee", age: 20, favourite_mums_dish: "Garlic Bread", username: "D", password: "123")
+      expect(post "/api/v1/users", params: user.to_json, headers: { 'Accept': "application/json", 'Content-Type': "application/json" }).to change(User, :count).by(1)
+      # expect(post "/api/v1/users", params: user).to change(User, :count).by(1)
     end
   end
 end
