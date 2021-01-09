bbdc09f2c1edd70f2634eb3296fbbcdc9ef37e91
BenDunjay <bendunjay@outlook.com>
2021-01-09 18:48:35 +0000

    create a recipe for a specific user

--- spec/requests/recipes_request_spec.rb
+++ spec/requests/recipes_request_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Recipes", type: :request do
-  let(:recipe) { create :recipe }
+  let(:user) { create :random_user }
+  let(:recipe) { create :recipe, user_id: user.id }
 
   context "when looking through all recipes" do
     it "accesses all recipes" do
@@ -22,7 +23,7 @@ RSpec.describe "Recipes", type: :request do
 
   describe "create" do
     it "creates a new recipe" do
-      recipe = { recipe: { name: "recipe_100", difficulty: "difficult", instructions: "Long instructions 100", cooking_time: 100, user_id:  } }
+      recipe = { recipe: { name: "recipe_100", difficulty: "difficult", instructions: "Long instructions 100", cooking_time: 100, user_id: user.id } }
       post "/api/v1/recipes", params: recipe.to_json, headers: { 'Accept': "application/json", 'Content-Type': "application/json" }
       body = JSON.parse(response.body)
       expect(response).to have_http_status :created
