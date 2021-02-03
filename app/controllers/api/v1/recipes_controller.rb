b44cd6b1045187d40b61982d1faf37ec72566250
BenDunjay <bendunjay@outlook.com>
2021-02-03 21:14:58 +0000

    added delete for recieps

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -33,6 +33,15 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def destroy
+    byebug
+    if logged_in_user
+      all_recipes = logged_in_user.recipes
+      recipe = all_recipes.find_by(id: params[:id])
+      recipe.destroy
+      render json: recipe, serializer: RecipeSerializer
+    else
+      render json: { error: "Unfortunately this is not your recipe to delete" }, status: :not_acceptable
+    end
   end
 
   private
