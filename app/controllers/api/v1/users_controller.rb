43f8c49f56415fc1b7dec0c4188344253a5fbf35
BenDunjay <bendunjay@outlook.com>
2021-02-03 21:20:25 +0000

    add users show controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,6 +6,11 @@ class Api::V1::UsersController < ApplicationController
     render json: users, each_serializer: UserSerializer
   end
 
+  def show
+    user = User.find(params[:id])
+    render json: user, serializer: UserSerializer
+  end
+
   def create
     user = User.create(user_params)
     if user.valid?
