f67c8e0dc0716d11a345b708bad145fec246340d
BenDunjay <bendunjay@outlook.com>
2021-03-13 21:42:09 +0000

    changes

--- spec/requests/recipes_request_spec.rb
+++ spec/requests/recipes_request_spec.rb
@@ -14,16 +14,7 @@ RSpec.describe "Recipes", type: :request do
     end
   end
 
-  # describe "create" do
-  #   it "creates a new recipe" do
-  #     recipe = { recipe: { name: "recipe_100", difficulty: "difficult", instructions: "Long instructions 100", cooking_time: 100, user_id: user.id } }
-  #     post "/api/v1/recipes", params: recipe.to_json, headers: { 'Accept': "application/json", 'Content-Type': "application/json" }
-  #     body = JSON.parse(response.body)
-  #     expect(response).to have_http_status :created
-  #   end
-  # end
-
-  describe "show" do
+  context "show" do
     it "shows the recipe" do
       get("/api/v1/recipes/#{recipe.id}")
       expect(response).to be_successful
--- spec/requests/users_recipes_request_spec.rb
+++ spec/requests/users_recipes_request_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "UsersRecipes", type: :request do
   let(:user) { create :random_user }
   let(:recipe) { create :recipe, user_id: user.id }
 
-  describe "create" do
+  context "create" do
     it "creates a new recipe" do
       recipe = { recipe: { name: "recipe_100", difficulty: "difficult", instructions: "Long instructions 100", cooking_time: 100, user_id: user.id } }
       post "/api/v1/users/#{user.id}/create_recipe", params: recipe.to_json, headers: { 'Accept': "application/json", 'Content-Type': "application/json" }
@@ -14,7 +14,6 @@ RSpec.describe "UsersRecipes", type: :request do
   end
 
   context "when logged in" do
-    # pending "cannot get the correct controller, error is, cannot find random_user id"
     it "shows the current users recipes" do
       get "/api/v1/users/#{user.id}/my_recipes"
       expect(response).to be_successful
