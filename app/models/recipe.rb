ccb52555be19b0a6213f21a2159485dbacf6986b
BenDunjay <bendunjay@outlook.com>
2021-03-15 16:34:57 +0000

    added bullet gem and checked current

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ApplicationRecord
-  # has_many :ingredients
   belongs_to :user
 
   validates_presence_of :name
   validates_presence_of :difficulty
   validates_presence_of :cooking_time
   validates_presence_of :instructions
+
 end
